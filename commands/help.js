d46e79526e0832ee52579823f5e36bb751bc8c4c
Patryk L <19snapy@users.noreply.github.com>
2019-05-22 23:45:21 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -14,12 +14,10 @@ module.exports =
 		.setColor("#8904B1")
 		.setTitle("Commands:")
 		.addField(`${prefix}help`, "Opens this menu.")
-		.addField(`${prefix}changeprefix`, `Changes current prefix for bot.\nSyntax: ${prefix}changeprefix <newprefix>`)
 		.addField(`${prefix}ping`, "Pong.")
 		.addField(`${prefix}userinfo`, `Displays information about specified user (Use the specified users id)\nSyntax: ${prefix}userinfo for self, ${prefix}userinfo <discordID>`)
 		.addField(`${prefix}leaguetrack`, `Gives information about specified summoner. \nSyntax: ${prefix}leaguetrack <region> <summonername without spaces>`)
 		.addField(`${prefix}csgotrack`, `Gives information about specified steamID. \nSyntax: ${prefix}csgotrack <steamID>`)
-		.addField(`${prefix}register or ${prefix}unregister`, `Associates or Disassociates discord ID to the specified game user (Put in 'steam' if its a steam game). \nSyntax" ${prefix}register <game> <discordID> <ingamename or id>`)
 		})
 	}
 };
